680d211e8dfb7f7f737d9446e3a9b859434b69e0
Vamshi925 <123926132+Vamshi925@users.noreply.github.com>
2026-07-22 17:53:40 +0530

    Refactor security configuration to use default CORS
    Removed custom CORS configuration bean and updated security filter chain to use default CORS settings.

--- medical-history-service/src/main/java/com/cts/medicalhistoryservice/security/SecurityConfig.java
+++ medical-history-service/src/main/java/com/cts/medicalhistoryservice/security/SecurityConfig.java
@@ -32,10 +32,11 @@ public class SecurityConfig {
      * @throws Exception If an error occurs during configuration.
      */
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.
-        		cors(cors->cors.configurationSource(corsConfigurationSource))
+        return http
+        		.cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
         		.csrf(customizer -> customizer.disable()).
                 authorizeHttpRequests(request -> request
 
@@ -58,19 +59,6 @@ public class SecurityConfig {
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
-    }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000"); // Allow frontend requests
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow HTTP methods
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept")); // Define headers
-        configuration.setAllowCredentials(true); // Allow cookies/session headers
- 
-        // Register configuration
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
 }
