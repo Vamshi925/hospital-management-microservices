4cbbee11b5e3d2e6e7e39bdd4d400c9f76cc6ba7
Vamshi925 <kvamshireddy5@gmail.com>
2026-07-22 13:30:04 +0000

    Added security to api gateway

---
+++ api-gateway/src/main/java/com/example/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.example.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        return http
+                // Disable CSRF for APIs
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+
+                // Enable CORS
+                .cors(Customizer.withDefaults())
+
+                // Authorization Rules
+                .authorizeExchange(exchange -> exchange
+
+                        // Allow browser preflight requests
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+
+                        // Allow actuator
+                        .pathMatchers("/actuator/**").permitAll()
+
+                        // Allow everything for now
+                        .anyExchange().permitAll()
+                )
+
+                .build();
+    }
+}
